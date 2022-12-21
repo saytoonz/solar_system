d4e3eb5436617626c57358a2d793ea95592898c4
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2022-12-21 19:04:06 +0000

    Update app_colors.dart

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF000000);
+  static const Color grey = Color.fromARGB(255, 105, 105, 105);
 }
